0d01f3cdab5156bad4995fbfc634e1788295ebe1
Kasper Henningsen <khenni20@student.aau.dk>
2020-11-17 12:59:35 +0100

    Update actual-code.c

--- actual-code.c
+++ actual-code.c
@@ -4,5 +4,6 @@ int main (void) {
     printf("Now, this is pod racing!\n");
     printf("Danny D knows whats up");
     printf("Nikolaj er mongol,\n");
+    printf("Nikolaj er en fucking stor mongol!!\n");
     return 1;
 }
