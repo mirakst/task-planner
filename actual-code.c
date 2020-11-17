b72d5c97b78907793cf84d57cce4228e09d8d3d4
mirakst <mstids20@student.aau.dk>
2020-11-17 13:00:37 +0100

    Added descriptive text of Nikolaj

--- actual-code.c
+++ actual-code.c
@@ -4,5 +4,6 @@ int main (void) {
     printf("Now, this is pod racing!\n");
     printf("Danny D knows whats up");
     printf("Nikolaj er mongol,\n");
+    printf("Nikolaj er en dreng på 12 år :)");
     return 1;
 }
