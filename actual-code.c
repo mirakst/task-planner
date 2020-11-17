8788996537d7bbf7dd024df506e93e79b702ad87
mirakst <mstids20@student.aau.dk>
2020-11-17 13:32:35 +0100

    Added function "test_function"

--- actual-code.c
+++ actual-code.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+int test_function (int);
 
 int main (void) {
+    int num = 69;
     printf("Now, this is pod racing!\n");
     printf("Danny D knows whats up");
     printf("Nikolaj er mongol,\n");
     printf("Nikolaj er en dreng på 12 år :)");
     printf("Nikolajs mor er 24 år gammel.\n");
     printf("Nikolaj er en fucking stor mongol!!\n");
+    num = test_function(num) * 2;
+    printf("Num multiplied by 2 is: %d", num);
     return 1;
+}
+
+int test_function (int num) {
+    printf("The number is: %d.\n", num);
+    return num + 1;
 }
