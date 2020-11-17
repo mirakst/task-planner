3c9ffa12d3b1acad551999bc11cb27242add00d8
Kasper Henningsen <khenni20@student.aau.dk>
2020-11-17 13:32:00 +0100

    Update actual-code.c

--- actual-code.c
+++ actual-code.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void fuckdam(void);
+
 int main (void) {
     printf("Now, this is pod racing!\n");
     printf("Danny D knows whats up");
@@ -7,5 +9,15 @@ int main (void) {
     printf("Nikolaj er en dreng på 12 år :)");
     printf("Nikolajs mor er 24 år gammel.\n");
     printf("Nikolaj er en fucking stor mongol!!\n");
+
+    fuckdam();
+
     return 1;
+}
+
+
+
+void fuckdam(void){
+    printf("Fuck DamDumDude");
+    return
 }
