c3963b6c7c46c6107736b726d702f7283d7cc8f7
mirakst <mstids20@student.aau.dk>
2020-11-18 15:13:41 +0100

    Built basic framework from my current understanding of file handling in C.

--- Calculate_Prices.c
+++ Calculate_Prices.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main (void) {
-    printf("Test!\n");
-    return 1;
-}
+    FILE *fp;
+    char str[8];
+    
+    fp =  fopen("files.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return -1;
+    }
+    if (fgets(str, 60))
