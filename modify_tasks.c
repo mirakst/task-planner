35e9a7aaf63b356fe5f5b0ddd241e4bc46f857b6
Nikolaj <ndam20@student.aau.dk>
2020-11-19 13:30:42 +0100

    Improved add/remove functionality, added list sorting

--- modify_tasks.c
+++ modify_tasks.c
@@ -1,5 +1,11 @@
 /* Navne - Mails? - Gruppe - Software */
-/* For plebs  <      > ellers anders røv {   } for the cool kids -=xXx<({[L 3 3 7]})>xXx=- */
+/* TODO: 
+ * Test-cases (too long task name, char instead of int, etc...)
+ * Save and load to file....
+ * Print name of task instead of id when removing
+ * Måske: sortere liste alfabetisk (eller efter bruger-indstilling?)
+ * Bedre variable-navne
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,8 +13,9 @@
 #include <ctype.h>
 
 #define SENTINEL "exit"
+#define EMPTY_TASK "none"
 #define TASK_NAME_MAX 20
-#define MAX_TASKS 10
+#define TASK_AMOUNT_MAX 10
 #define INPUT_MAX 50
 
 struct task {
@@ -18,92 +25,98 @@ struct task {
 };
 typedef struct task task;
 
+/* From main.c - converts string to lower-case */
 void string_to_lower(char *str) {
     int i;
     for (i = 0; i < INPUT_MAX; i++)
         str[i] = tolower(str[i]);
 }
 
+/* Prototypes */
 void Print_List(task *, int);
 task Add_Task(void);
 task Add_Task_DEBUG(char *, int, int);
 task Remove_Task(void);
 void Print_Help (void);
+void Sort_Tasks (task *);
+int Task_Compare (const void *, const void *);
 
 int main (void) {
-    task task_list[MAX_TASKS];
+    task task_list[TASK_AMOUNT_MAX];
     char cmd_input[INPUT_MAX];
     int task_amount = 0,
         task_id = 0;
 
-    task_list[0] = Add_Task_DEBUG("Opvasker", 55, 180);
-    task_list[1] = Add_Task_DEBUG("Vaskemaskine", 45, 180);
-    task_list[2] = Add_Task_DEBUG("Stoevsuger", 35, 30);
-    task_list[3] = Add_Task_DEBUG("Haandmixer", 5, 10);
-    task_list[4] = Add_Task_DEBUG("Mikrovn", 30, 5);
+    /* Initialize tasks to be empty */
+    for (int i = 0; i < TASK_AMOUNT_MAX; i++) {
+        task_list[i] = Remove_Task();
+    }
+    
+    /* Adds 5 different tasks [FOR DEBUGGING] */
+    task_list[0] = Add_Task_DEBUG("Opvaskning", 55, 180);
+    task_list[1] = Add_Task_DEBUG("Vaskemaskining", 45, 180);
+    task_list[2] = Add_Task_DEBUG("Stoevsugning", 35, 30);
+    task_list[3] = Add_Task_DEBUG("zzzzzzzzzz", 5, 10);
+    task_list[4] = Add_Task_DEBUG("Mikroovning", 30, 5);
     task_amount = 5;
 
     printf("\n---------------- Modify tasks ----------------\n");
 
-    do
-    {
+    do {
         /* Get user input and convert to lower case letters */
         printf("\nPlease enter a command: ");
         scanf(" %s", cmd_input);
         string_to_lower(cmd_input);
 
-        /* If "add": Get info and add the task*/
         if(!strcmp(cmd_input, "add")) {
-            if(task_amount == MAX_TASKS) {
+            if(task_amount == TASK_AMOUNT_MAX) {
                 printf("The maximum amount of tasks has been reached.\n");
             }
             else {
                 task_list[task_amount] = Add_Task();
+                printf("Task %d was successfully added.\n", task_amount + 1);
                 task_amount++; 
             }
         }
 
-        if(!strcmp(cmd_input, "list"))
+        if (!strcmp(cmd_input, "list"))
             Print_List(task_list, task_amount);
         
-        if(!strcmp(cmd_input, "help"))
+        if (!strcmp(cmd_input, "help"))
             Print_Help();
 
-        if(!strcmp(cmd_input, "remove")) {
-            
-            printf("Enter task id to remove: ");
-
-            if(scanf(" %d", &task_id) == 0) {
-                printf("Wrong command fuckhead\n");
+        if (!strcmp(cmd_input, "remove")) {
+            if (scanf(" %d", &task_id) == 0 || task_id < 1 || task_id > task_amount) {
+                printf("The id was not recognized.\n");
             } 
             else {
                 task_list[(task_id - 1)] = Remove_Task();
-
-                if(!strcmp(task_list[(task_id - 1)].name, "NULL"))
+                if (!strcmp(task_list[(task_id - 1)].name, EMPTY_TASK))
                     printf("Task %d was successfully removed.\n", task_id);
+                
+                task_amount--;
+                Sort_Tasks(task_list);
             }
         }
-        
+
+
     } while (strcmp(cmd_input, SENTINEL) != 0);
     
     return EXIT_SUCCESS;
 }
 
-void Modify_Tasks () {
-    
-}
-
+/* Prints all the available commands  */
 void Print_Help (void) {
     printf("\n------------------ Commands ------------------\n");
-    printf("%-12s -- Lists all tasks.\n", "List");
-    printf("%-12s -- Add a new task.\n", "Add");
-    printf("%-12s -- Remove a task\n", "Remove");
-    printf("%-12s -- Go back to main.\n", "Back");
+    printf("%-11s -- Lists all tasks.\n", "List");
+    printf("%-11s -- Add a new task.\n", "Add");
+    printf("%-11s -- Remove a task\n", "Remove (id)");
+    printf("%-11s -- Go back to main.\n", "Back");
     printf("----------------------------------------------\n");
-    return;
 }
 
-task Add_Task(void) {
+/* Gets  input from the user and returns it as a task */
+task Add_Task (void) {
     task result;
     char name[TASK_NAME_MAX];
     int power = 0, use_time = 0;
@@ -121,7 +134,8 @@ task Add_Task(void) {
     return result;
 }
 
-task Add_Task_DEBUG(char *name, int power, int use_time) {
+/* Returns a task with the given input parameters [FOR DEBUGGING] */
+task Add_Task_DEBUG (char *name, int power, int use_time) {
     task result;
     strcpy(result.name, name);
     result.power = power;
@@ -129,32 +143,42 @@ task Add_Task_DEBUG(char *name, int power, int use_time) {
     return result;
 }
 
-/* Få sorteret arrayet efter det er fjernet, for at få NULL til sidst */
-task Remove_Task(void) {
+/* Returns an 'empty' task */
+task Remove_Task (void) {
     task result;
-    strcpy(result.name, "NULL");
+    strcpy(result.name, EMPTY_TASK);
     result.power = 0;
-    result.use_time = 0;
+    result.use_time = 0;    
     return result;
 }
 
-/*void Write_To_File() {
-
+/* Sorts the input array with qsort and the Task_Compare function */
+void Sort_Tasks (task *task_list) {
+    qsort(task_list, TASK_AMOUNT_MAX, sizeof(task), Task_Compare);
 }
 
-void Read_File() {
-    
-}*/
+/* Compares two tasks, and lets a non-empty task come first */
+int Task_Compare (const void *ip1, const void *ip2) {
+    const task *task1 = (task *) ip1,
+               *task2 = (task *) ip2;
 
+    if(strcmp(task1->name, EMPTY_TASK) == 0 && strcmp(task2->name, EMPTY_TASK) != 0)
+        return 1;
+    else if(strcmp(task1->name, EMPTY_TASK) != 0 && strcmp(task2->name, EMPTY_TASK) == 0)
+        return -1;
+    else
+        return 1;
+}
+
+/* Prints the list of all tasks, excluding the empty tasks */
 void Print_List(task *task_list, int task_amount) {
     int i;
     printf("\n----------------- Task  list -----------------\n");  
     printf("%13s%22s%11s\n", "Name", "Power", "Use time");    
-    for (i = 0; i < task_amount; i++) {
-        if (strcmp(task_list[i].name, "NULL") != 0) {
+    for (i = 0; i < TASK_AMOUNT_MAX; i++) {
+        if (strcmp(task_list[i].name, EMPTY_TASK) != 0) {
             printf("Task %2d: %-20s %3d W %6d min\n", (i + 1), task_list[i].name, task_list[i].power, task_list[i].use_time);
         }
     }
     printf("----------------------------------------------\n");
 }
-
