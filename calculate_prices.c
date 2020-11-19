5e7139884f258558f3e23e89411406e5294af6fa
mirakst <mstids20@student.aau.dk>
2020-11-19 11:46:09 +0100

    Isolated function "Load_Prices()"

--- calculate_prices.c
+++ calculate_prices.c
@@ -3,33 +3,27 @@
 #define HOURS_PR_DAY 24
 
 int cmp (const void *, const void *);
+void Load_Prices (char*, FILE *fp, double prices[][2]);
 
 int main (void) {
-    FILE *fp;
+    FILE *fp = fopen("prices.txt", "r");
     char str[13];
     int i,
         pos;
     double prices[HOURS_PR_DAY][2];
 
-    
-    fp =  fopen("prices.txt", "r");
     if (fp == NULL) {
         printf("Error opening file.\n");
         return -1;
     }
-    for (i = 0; i < HOURS_PR_DAY; i++) {
-        fgets(str, 13, fp);
-        printf("[DEBUG] The price fetched is: %s", str);
-        prices[i][1] = i;
-        prices[i][0] = strtod(str, str+6);
-    }
-    printf("\n");
-    printf("\n");
+
+    Load_Prices (str, fp, prices);
+
     for (i = 0; i < HOURS_PR_DAY; i++) {
         printf("[DEBUG] The price stored in slot [%d] is: %.2f\n", i, prices[i][0]);
     }
-    
     printf("\n");
+
     qsort(prices, 24, 2*(sizeof(double)), cmp);
     
     for (i = 0; i < HOURS_PR_DAY; i++) {
@@ -45,4 +39,16 @@ int cmp(const void *x, const void *y)
   if (xx < yy) return -1;
   if (xx > yy) return  1;
   return 0;
+}
+
+void Load_Prices (char* str, FILE *fp, double prices[][2]) {
+    int i;
+    for (i = 0; i < HOURS_PR_DAY; i++) {
+        fgets(str, 13, fp);
+        printf("[DEBUG] The price fetched is: %s", str);
+        prices[i][1] = i;
+        prices[i][0] = strtod(str, str+6);
+    }
+    printf("\n\n");
+    return;
 }
