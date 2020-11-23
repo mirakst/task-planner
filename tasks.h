8d81c68e38163312e7623c8a5c8ad737bdac2b5c
Nikolaj <ndam20@student.aau.dk>
2020-11-23 15:01:49 +0100

    Added header file and simplified the functionality

---
+++ tasks.h
@@ -0,0 +1,21 @@
+#define FILE_TASKLIST "tasklist.txt"
+#define TASK_AMOUNT_MAX 10
+#define TASK_NAME_MAX 20
+#define EMPTY_TASK_NAME "none"
+
+struct task {
+    char name[TASK_NAME_MAX];
+    int power;
+    int duration;
+    double watt_hrs;
+};
+typedef struct task task;
+
+void Initialize_Tasks(task *, int *);
+int Load_Tasks (task *, int *);
+int Save_Tasks (task *, int);
+void Print_List(task *, int );
+void Add_Task (task *, int *);
+void Remove_Task (task *, int *, int);
+void Sort_Task_List (task *);
+int Compare_Tasks (const void *, const void *);
