bc0bf7ff3183b741fcd0f55f406faa901aec1cb5
Anders Mariegaard <Amarie20@student.aau.dk>
2020-11-23 15:44:59 +0100

    Added saving functionality aswell as wrapping the time in Get_available Hours

--- Get_User_Details.c
+++ Get_User_Details.c
@@ -1,39 +1,33 @@
-/* TODO: 
-    int for start og slut tiderne, sæt dem i et for-loop, og loop tiden efter den 23 time til 0 
-    24 bools i et array der siger om timen er åben eller ej
-    dialogtræ om redigering af profilen
-    
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #define INPUT_MAX 20
 
-enum command {cmd_exit, cmd_help, cmd_unrecognized, name, hours};
+enum command {cmd_exit, cmd_help, name, hours, save, cmd_unrecognized};
 typedef enum command command;
 
 void string_to_lower(char *);
 int compare_command(char *);
 
-void Get_Name (char *);
-void Get_available_Hours (unsigned int time[][1]);
-//void Make_Timetable (unsigned int *time);
-
-
-
 struct User_Details {
-    unsigned int available_schedule[24][1];
-    char user_name [];
+    int available_schedule[24][1];
+    char user_name [100];
 };
+typedef struct User_Details User;
+
+void Get_Name (User *);
+void Get_available_Hours (int time[][1]);
+void Save_To_File (User User);
+//void Make_Timetable (int *time);
+
 
 int main (void) {
-    struct User_Details User;
+    User user;
     char cmd_input[INPUT_MAX];
     int cmd;
-
-
+    
+    int bool_hours = 0, bool_name = 0;
 
     do {
         /* Get user input and convert to lower case letters */
@@ -42,8 +36,6 @@ int main (void) {
         string_to_lower(cmd_input);
         cmd = compare_command(cmd_input);
         
-        printf("[DEBUG]: cmd = %d: %s\n", cmd, cmd_input); /* For debugging */
-        
         /* Run the appropriate function based on the command */
         switch (cmd) {
             case cmd_exit:
@@ -51,23 +43,32 @@ int main (void) {
                 break;
             
             case cmd_help:
-                printf("Printing all available commands.\n");
+                printf("Printing all available commands:\n name\n hours\n save\n exit\n");
                 break;
 
             case name:
-                Get_Name (User.user_name);
-                printf("Hello %s\n", User.user_name);
+                Get_Name (&user);
+                printf("Hello %s\n", user.user_name);
+                bool_name = 1;
                 break;
 
             case hours:
-                Get_available_Hours (User.available_schedule);
+                Get_available_Hours (user.available_schedule);
+                bool_hours = 1;
+                break;
+
+            case save:
+                if (bool_hours == 1 && bool_name == 1) 
+                    Save_To_File(user);
+                else 
+                    printf("You have to set a name and time of unavailability to save\n");
                 break;
 
             default:
                 printf("Command not recognized.\n");
                 break;
         }
-
+        
     } while (cmd != cmd_exit);
 
     return EXIT_SUCCESS;
@@ -90,43 +91,58 @@ int compare_command (char *str) {
         return name;
     else if (!strcmp(str, "hours"))
         return hours;
+    else if (!strcmp(str, "save"))
+        return save;
     else
         return cmd_unrecognized;
 }
 
 /* Takes the name of the user */
-void Get_Name (char *name) {
+void Get_Name (User *user) {
 
     printf("Please enter your name: ");
-    scanf("%s", name);
+    scanf("%s", user->user_name);
 
 }
 
 /* Adds the available times to an array for easy access */
-void Get_available_Hours (unsigned int time[][1]) {
+void Get_available_Hours (int time[][1]) {
     int i;
-    unsigned int start_time, end_time;
+    int start_time, end_time;
 
-    printf("Enter an interval of when you've unavailable between 0 and 23: ");
-    scanf("%u %u", &start_time, &end_time);
+    printf("Enter an interval where you're unavailable between 0 and 23: ");
+    scanf("%d %d", &start_time, &end_time);
 
     for (i = 0; i < 24; i++) {
-        if (start_time > end_time) {
-            
+        /* Wrap time if the interval goes from one day to another */
+        if (start_time >= end_time) {
+            if (i >= start_time && i < 24 || i <= end_time && i >= 0) {
+                time[i][0] = 1;
+            }
+            else time[i][0] = 0;
         }
         
-        
-        if (i >= start_time && i <= end_time) {
-            time[i][1] = 1;
+        /* Sets the boolean to true if i is in the interval */
+        if (start_time <= end_time) {
+            if (i >= start_time && i <= end_time) {
+                time[i][0] = 1;
+            }
+            else time[i][0] = 0;
         }
-        else time[i][1] = 0;
-
-        printf("%u - %.2d\n", time[i][1], i);
     }
-
 }
 
-/* Creates a visualized timetable based on the available hours */
-/*void Make_Timetable (unsigned int *time) {
+void Save_To_File (User user) {
+    int i = 0;
+    FILE *p_File;
 
-}*/
+    p_File = fopen("User_Details.txt", "w");
+    fprintf(p_File, "%s\n", user.user_name);
+
+    for (i = 0; i < 24; i++) {
+        fprintf(p_File, "%d-%d\n", i, user.available_schedule[i][0]);
+    }
+
+    fclose(p_File);
+    printf("Finished saving...\n");
+}
