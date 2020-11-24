6256f8a4f2852525bd346b51bad575a8d7b59602
mirakst <74593818+mirakst@users.noreply.github.com>
2020-11-24 09:22:46 +0100

    added #defines

--- calculate_prices.h
+++ calculate_prices.h
@@ -1,3 +1,6 @@
+#define HOURS_PR_DAY 24
+#define PRICES_FILE "prices.txt"
+
 int Double_Compare (const void *, const void *);
 void Load_Prices (char*, FILE*, double[][2]);
 void Calculate_Prices (double[][2]);
