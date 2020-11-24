15a92349b6619c82dbbcbbc6fe88b21cac376715
Anders Mariegaard <Amarie20@student.aau.dk>
2020-11-24 10:50:05 +0100

    Getting ready to pull request, deleted main and the command functionality, created Get_User_Details.h with the prototypes.

--- Get_User_Details.c
+++ Get_User_Details.c
@@ -1,99 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#define INPUT_MAX 20
+#include "Get_User_Details.h"
 
-enum command {cmd_exit, cmd_help, name, hours, reset, save, cmd_unrecognized};
-typedef enum command command;
-
-void string_to_lower(char *);
-int compare_command(char *);
-
-typedef struct User_Details {
-    int available_schedule[24][1];
-    char user_name [100];
-}User;
-
-void Get_Name (User *, int *);
-void Reset_available_Hours (int time[][1]);
-void Get_available_Hours (int time[][1], int *);
-void Save_To_File (User, int, int);
-
+/* Append in main.c
 int main (void) {
     User user;
-    char cmd_input[INPUT_MAX];
-    int cmd;
-    
     int bool_hours = 0, bool_name = 0;
+}*/
 
-    do {
-        /* Get user input and convert to lower case letters */
-        printf("Please enter a command: ");
-        scanf(" %s", cmd_input);
-        string_to_lower(cmd_input);
-        cmd = compare_command(cmd_input);
-        
-        /* Run the appropriate function based on the command */
-        switch (cmd) {
-            case cmd_exit:
-                printf("Exiting.\n");
-                break;
-            
-            case cmd_help:
-                printf("Printing all available commands:\n name\n hours\n save\n exit\n");
-                break;
-
-            case name:
-                Get_Name (&user, &bool_name);
-                break;
-
-            case hours:
-                Get_available_Hours (user.available_schedule, &bool_hours);
-                break;
-
-            case reset:
-                Reset_available_Hours (user.available_schedule);
-                break;
-
-            case save:
-                Save_To_File(user, bool_name, bool_hours);
-                break;
-
-            default:
-                printf("Command not recognized.\n");
-                break;
-        }
-        
-    } while (cmd != cmd_exit);
-
-    return EXIT_SUCCESS;
-}
-
-/* string to lower from main.c */
-void string_to_lower(char *str) {
-    int i;
-    for (i = 0; i < INPUT_MAX; i++)
-            str[i] = tolower(str[i]);
-}
-
-/* Command compare function from main.c */
-int compare_command (char *str) {
-    if (!strcmp(str, "help"))
-        return cmd_help;
-    else if (!strcmp(str, "exit"))
-        return cmd_exit;
-    else if (!strcmp(str, "name"))
-        return name;
-    else if (!strcmp(str, "hours"))
-        return hours;
-    else if (!strcmp(str, "reset"))
-        return reset;
-    else if (!strcmp(str, "save"))
-        return save;
-    else
-        return cmd_unrecognized;
-}
+/* Calls to the functions
+Get_Name (&user, &bool_name);
+Get_available_Hours (user.available_schedule, &bool_hours);
+Reset_available_Hours (user.available_schedule);
+Save_To_File(user, bool_name, bool_hours);
+*/
 
 /* Takes the name of the user */
 void Get_Name (User *user, int *bool_name) {
@@ -104,15 +24,17 @@ void Get_Name (User *user, int *bool_name) {
     *bool_name = 1;
 }
 
-void Reset_available_Hours (int time[][1]) {
+/* Function for resetting the unavailable hours */
+void Reset_Available_Hours (int time[][1]) {
     int i;
     for (i = 0; i < 24; i++) {
         time[i][0] = 0;
     }
+    printf("Reset complete\n")
 }
 
 /* Adds the available times to an array for easy access */
-void Get_available_Hours (int time[][1], int *bool_hours) {
+void Get_Available_Hours (int time[][1], int *bool_hours) {
     int i;
     int start_time, end_time;
 
@@ -135,19 +57,20 @@ void Get_available_Hours (int time[][1], int *bool_hours) {
             break;
         }
 
+        /* Sets the boolean to true if i is in the interval of start_time to end_time */
+        else if (start_time < end_time) {
+            if (i >= start_time && i <= end_time) {
+                time[i][0] = 1;
+            } 
+        }
+
         /* Wrap time if the interval goes from one day to another */
         else if (start_time > end_time) {
             if ((i >= start_time && i < 24) || (i <= end_time && i >= 0)) {
                 time[i][0] = 1;
             }
         }
-        
-        /* Sets the boolean to true if i is in the interval */
-        else if (start_time < end_time) {
-            if (i >= start_time && i <= end_time) {
-                time[i][0] = 1;
-            } 
-        }
+        printf("Hour %d - %d", i, time[i][0]);
     }
 
     /* boolean keeping track of whether the hours have been set */
@@ -159,7 +82,7 @@ void Save_To_File (User user, int bool_name, int bool_hours) {
     if (bool_hours == 1 && bool_name == 1) {
     int i = 0;
     FILE *p_File;
-    p_File = fopen("User_Details.txt", "w");
+    p_File = fopen(FILE_USER_DETAILS, "w");
     fprintf(p_File, "%s\n", user.user_name);
 
     /* The 24 hours in the day and their boolean value*/
